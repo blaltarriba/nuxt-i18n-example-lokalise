8c72cf7157f381e4df56ca88e27632c22e4e2f57
Borja Lopez <borja.lopez@numan.com>
2022-04-22 09:29:06 +0200

    Add text only in en file

--- lang/en_US.js
+++ lang/en_US.js
@@ -2,7 +2,8 @@ var translations = {
     "home": "Homepage",
     "posts": "Posts",
     "about": "About us",
-    "homepage.subtitle": "nuxt-i18n demo"
+    "homepage.subtitle": "nuxt-i18n demo",
+    "homepage.subtitle2": "nuxt-i18n demo2"
 };
 
 export default translations;
