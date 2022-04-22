d5c5d7a0461cb220a6da0e9ef41828dd7b497537
Borja Lopez <borja.lopez@numan.com>
2022-04-22 12:46:56 +0200

    change list

--- lang/en_US/en_US-account.js
+++ lang/en_US/en_US-account.js
@@ -9,33 +9,15 @@ export default {
   "key6": [
     {
       "title": "How do you take antacids?",
-      "text": "Antacids should be taken when you feel the symptoms of reflux. Usually, this is just after meals or before you go to bed.<br><br>\
-        It’s recommended to take antacids as advised by a clinician or as directed in the patient information leaflet that comes with your medication. If you are prescribed any other medication, you should allow a 2-hour window on either side of taking an antacid, as it can affect absorption in the gut.",
+      "text": "Antacids the gut.",
     },
     {
       "title": "Do antacids have side effects?",
-      "text": "Antacids generally have no side effects for most people when they are taken in the right way.<br><br>\
-        There is a very rare chance of an allergic reaction that occurs in less than 1 in 10,000 patients. If symptoms such as swelling or a rash occur, you should stop taking the medication and consult a clinician.",
+      "text": "Antacids generally.",
     },
     {
       "title": "Will I need antacids long term?",
-      "text": "Antacids are fine to take occasionally for symptoms of heartburn if overseen by a health professional.<br><br/>\
-        If you need an antacid for more than 7 days in a row, it’s recommended to check in with a clinician to rule out any underlying conditions and to make sure that you’re on the best treatment plan for you.",
-    },
-    {
-      "title": "Is there anything else I can do to help acid reflux?",
-      "text": "Some lifestyle factors can lead to acid reflux or can make symptoms worse.<br><br>\
-        These can include:<br><br>\
-        <ul>\
-          <li>Smoking</li>\
-          <li>Some prescription or over the counter medications (most commonly drugs from a family called NSAIDs which includes ibuprofen)</li>\
-          <li>Eating fatty, salty, or spicy foods</li>\
-          <li> Eating large meals, or eating too quickly</li>\
-          <li> Being overweight</li>\
-          <li> Stress or anxiety</li>\
-          <li>Excessive caffeine or alcohol</li>\
-        </ul><br>\
-        <p>Our clinical team will review your lifestyle as part of your assessment, and can advise on the best course of action for you to manage your symptoms.</p>",
+      "text": "Antacids are fine .",
     },
   ],
 };
