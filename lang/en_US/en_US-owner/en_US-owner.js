9c2f5cc80b7b5d40dfad0136791b839dc927ef78
Borja Lopez <borja.lopez@numan.com>
2022-04-22 12:55:03 +0200

    Add more subfolders

---
+++ lang/en_US/en_US-owner/en_US-owner.js
@@ -0,0 +1,7 @@
+export default {
+  "key22": "Posts",
+  "key23": "About us",
+  "key24": {
+    "key25": "nuxt-i18n demo"
+  }
+};
---
+++ lang/fr_FR/fr_FR-owner/fr_FR-owner.js
@@ -0,0 +1,7 @@
+export default {
+  "key22": "",
+  "key23": "",
+  "key24": {
+    "key25": ""
+  }
+};
