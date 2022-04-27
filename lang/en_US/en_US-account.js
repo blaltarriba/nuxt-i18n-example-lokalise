86dc78bae6bfe39af8289047ca4546e82cb62759
Borja Lopez <borja.lopez@numan.com>
2022-04-27 09:47:00 +0200

    Add new file

--- lang/en_US/en_US-account.js
+++ lang/en_US/en_US-account.js
@@ -6,18 +6,4 @@ export default {
   "key4": {
     "key5": "nuxt-i18n demo"
   },
-  "key6": [
-    {
-      "title": "How do you take antacids?",
-      "text": "Antacids the gut.",
-    },
-    {
-      "title": "Do antacids have side effects?",
-      "text": "Antacids generally.",
-    },
-    {
-      "title": "Will I need antacids long term?",
-      "text": "Antacids are fine .",
-    },
-  ],
 };
