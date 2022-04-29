036ebe33fd8e80ce75146c39f929baeefe9ab39e
Borja López <borja.lopez83@gmail.com>
2022-04-29 11:21:37 +0200

    Translations ready

---
+++ i18n.js
@@ -0,0 +1,15 @@
+import I18n from 'react-native-i18n';
+
+import en_US from './lang/en_US/en_US-account.js'
+import fr_FR from './lang/fr_FR/fr_FR-account.js'
+import es_ES from './lang/es_ES/es_ES-account.js'
+
+I18n.fallbacks = true;
+
+I18n.translations = {
+    en_US,
+    fr_FR,
+    es_ES
+};
+
+export default I18n;
--- lang/fr_FR/fr_FR-account.js
+++ lang/fr_FR/fr_FR-account.js
@@ -1,8 +1,3 @@
 export default {
-  "key1": "",
-  "key2": "",
-  "key3": "",
-  "key4": {
-    "key5": ""
-  }
+    "key2": "Es deleted posts"
 };
